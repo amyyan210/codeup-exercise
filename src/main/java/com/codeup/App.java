5967dbb8afca1487b3cff865f05443f92e9faa4b
amyyan210 <amyyan210@gmail.com>
2017-10-16 10:06:08 -0500

    Start and complete dependencies exercise

--- src/main/java/com/codeup/App.java
+++ src/main/java/com/codeup/App.java
@@ -1,5 +1,9 @@
 package com.codeup;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
+
+
+//        Tells whether or not what the user entered is a number
+        if(StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is a number.");
+        } else {
+            System.out.println(userInput + " is not a number.");
+        }
+
+
+//        Flips the case of the string
+        System.out.println(StringUtils.swapCase(userInput));
+
+//        Reverses the string
+        System.out.println(StringUtils.reverse(userInput));
+
     }
 }
